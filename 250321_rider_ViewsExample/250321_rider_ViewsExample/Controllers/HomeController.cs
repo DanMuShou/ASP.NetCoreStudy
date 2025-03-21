f83fbe24ad3b665b599b71a062428d9f6e06d3c5
DanMuShou <stamt-ailant@qq.com>
2025-03-21 22:43:02 +0800

    a

--- 250321_rider_ViewsExample/250321_rider_ViewsExample/Controllers/HomeController.cs
+++ 250321_rider_ViewsExample/250321_rider_ViewsExample/Controllers/HomeController.cs
@@ -34,10 +34,45 @@ public class HomeController : Controller
             },
         ];
 
-        ViewData["persons"] = persons;
+        // ViewBag.persons = persons;
         // return View();
         // return new ViewResult() { ViewName = "Home" };
         // return View(); //Views/Home/Home.cshtml
-        return View(); //Views/Home/Home.cshtml
+        return View("Home", persons); //Views/Home/Home.cshtml
+    }
+
+    [Route("personDetails/{name}")]
+    public IActionResult Details(string? name)
+    {
+        if (name == null)
+            return NotFound("Person name can't be null");
+
+        List<Person> persons =
+        [
+            new Person()
+            {
+                Name = "Rider",
+                DateOfBirth = Convert.ToDateTime("1980-01-01"),
+                PersonGender = Gender.Female,
+            },
+            new Person()
+            {
+                Name = "Vs",
+                DateOfBirth = Convert.ToDateTime("1980-01-01"),
+                PersonGender = Gender.Female,
+            },
+            new Person()
+            {
+                Name = "VsCode",
+                DateOfBirth = null,
+                PersonGender = Gender.Female,
+            },
+        ];
+
+        var targetPerson = persons.FirstOrDefault(temp => temp.Name == name);
+        if (targetPerson == null)
+            return NotFound($"Person with name {name} not found");
+
+        return View("PersonDetails", targetPerson);
     }
 }
